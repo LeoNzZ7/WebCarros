5d04f68246a6746e6e8cfbf0d1f3e3eab45c0a64
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-08-14 19:42:56 -0300

    feat(header): implementar React Icons e finalizar componente
    - Adicionar biblioteca React Icons para melhorar a experiência do usuário
    - Finalizar implementação do componente Header
    - Otimizar uso de ícones para melhor desempenho e consistência visual

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom"
+import logo from "../../assets/logo.svg"
+import { FiLogIn, FiUser } from "react-icons/fi"
+
 export const Header = () => {
+    const signed = false
+    const loadingAuth = true
+
     return (
-        <div>
-            Header
+        <div className="w-full flex justify-center items-center h-16 bg-white drop-shadow mb-4" >
+            <header className="flex justify-between max-w-7xl items-center w-full px-4 mx-auto" >
+                <Link to="./" >
+                    <img src={logo} alt="logo do site" />
+                </Link>
+                {!loadingAuth && signed && (
+                    <Link to="/dashboard" className="rounded-full border-2 border-gray-900 p-1" >
+                        <FiUser size={22} color="#000" />
+                    </Link>
+                )}
+                {loadingAuth && !signed && (
+                    <Link to="/dashboard" className="rounded-full border-2 border-gray-900 p-1" >
+                        <FiLogIn size={22} color="#000" />
+                    </Link>
+                )}
+            </header>
         </div>
     )
 }
