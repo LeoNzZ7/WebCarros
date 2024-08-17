daa24690dae8b7fa83af765b39b1c509b0cebc37
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-08-17 16:27:59 -0300

    feat: Implementar contexto global para verificação de autenticação
    - Adicionado contexto global para gerenciar o estado de autenticação do usuário
    - Implementada lógica para verificar se o usuário está logado em toda a aplicação
    - Melhorada a segurança e a experiência do usuário com verificação de autenticação consistente

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,47 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { auth } from "../services/firebaseConnection";
+
+type AuthContextData = {
+    singed: boolean;
+    loadingAuth: boolean;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+interface UserProps {
+    uid: string
+    displayName: string | null
+    email: string | null
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+    const [user, setUser] = useState<UserProps | null>(null)
+    const [loadingAuth, setLoadingAuth] = useState(true)
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser({ uid: user.uid, displayName: user?.displayName, email: user?.email })
+                setLoadingAuth(false)
+            } else {
+                setUser(null)
+                setLoadingAuth(false)
+            }
+        })
+
+        return () => unsub()
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{ singed: !!user, loadingAuth }} >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
